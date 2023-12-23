1d282473deab09250379bf1a783b99b1d8e201b0
Daniël Terwiel <daniel@terwiel.io>
2023-12-23 11:47:18 +0100

    mapping: added moving lines up and down with ctrl j/k

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,3 +1,10 @@
 vim.g.mapleader = " " -- easy to reach leader key
 vim.keymap.set("n", "-", vim.cmd.Ex) -- need nvim 0.8+
 
+-- move lines up or down
+vim.api.nvim_set_keymap('n', '<C-j>', ':m .+1<CR>==', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', ':m .-2<CR>==', { noremap = true, silent = true })
+
+-- FIXME: select next occurrence of a selection
+-- vim.api.nvim_set_keymap('n', '<C-d>', '*``Nvgn', { noremap = true, silent = true })
+
