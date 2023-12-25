0f8f1b44f658e0ae9b3ffb645454296279cbaf8f
Daniël Terwiel <daniel@terwiel.io>
2023-12-25 12:51:56 +0100

    feat: always copy/cut/paste to system clipboard

--- lua/core/editor.lua
+++ lua/core/editor.lua
@@ -3,3 +3,5 @@ vim.o.shiftwidth = 2		-- Default tab width
 vim.wo.number = true        	-- Line numbers
 vim.wo.relativenumber = true  	-- Relative line numbers
 
+vim.o.clipboard = "unnamedplus" -- Always copy/cut/paste to clipboard
+
