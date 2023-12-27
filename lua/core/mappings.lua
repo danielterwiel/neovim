7f853641f4d4e625e1320de4239c674cee2f47f8
Daniël Terwiel <daniel@terwiel.io>
2023-12-27 13:03:02 +0100

    feat: vim-visual-multi select all

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,5 +1,5 @@
-vim.g.mapleader = " " -- easy to reach leader key
-vim.keymap.set("n", "-", vim.cmd.Ex) -- need nvim 0.8+
+-- easy to reach leader key
+vim.g.mapleader = " "
 
 -- toggle nvim-tree
 vim.api.nvim_set_keymap("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
@@ -8,11 +8,12 @@ vim.api.nvim_set_keymap("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = tru
 vim.api.nvim_set_keymap("n", "<C-j>", ":m .+1<CR>==", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-k>", ":m .-2<CR>==", { noremap = true, silent = true })
 
--- remap ctrl+d to half-page down scroll to ctrl+shift+d
-vim.api.nvim_set_keymap("n", "<C-d>", "<C-S-d>", { noremap = true, silent = true })
+-- remap ctrl+d to half-page down scroll to alt+d
+vim.api.nvim_set_keymap("n", "<C-d>", "<A-S-d>", { noremap = true, silent = true })
 
 -- map ctrl+d to vim-visual-multi to select next occurence of selection
 vim.g.VM_maps = {
 	["Find Under"] = "<C-d>",
 	["Find Subword Under"] = "<C-d>",
+	["Select All"] = "<C-S-d>",
 }
