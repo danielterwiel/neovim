618ec5add8095c8d850718c2b47fbc209db6e736
Daniël Terwiel <daniel@terwiel.io>
2023-12-27 16:18:49 +0100

    fix: prevent keymap collision for (multi-)line move

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -4,9 +4,13 @@ vim.g.mapleader = " "
 -- toggle nvim-tree
 vim.api.nvim_set_keymap("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 
--- move lines up or down
--- vim.api.nvim_set_keymap("n", "<C-j>", ":m .+1<CR>==", { noremap = true, silent = true })
--- vim.api.nvim_set_keymap("n", "<C-k>", ":m .-2<CR>==", { noremap = true, silent = true })
+-- Move line(s) down in normal and visual mode
+vim.api.nvim_set_keymap("n", "<A-j>", ":m .+1<CR>==", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("x", "<A-j>", ":move '>+1<CR>gv=gv", { noremap = true, silent = true })
+
+-- Move line(s) up in normal and visual mode
+vim.api.nvim_set_keymap("n", "<A-k>", ":m .-2<CR>==", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("x", "<A-k>", ":move '<-2<CR>gv=gv", { noremap = true, silent = true })
 
 -- remap ctrl+d to half-page down scroll to alt+d
 vim.api.nvim_set_keymap("n", "<C-d>", "<A-S-d>", { noremap = true, silent = true })
@@ -19,4 +23,6 @@ vim.g.VM_maps = {
 	["Select All"] = "<C-S-d>",
 	["Normal Yank"] = "y",
 	["Normal Paste"] = "P",
+	["Add Cursor Down"] = "<C-j>",
+	["Add Cursor Up"] = "<C-k>",
 }
