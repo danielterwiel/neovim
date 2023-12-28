3a531ff63de835dae4e6083af5728aab53cd43b6
Daniël Terwiel <daniel@terwiel.io>
2023-12-28 15:18:46 +0100

    chore: Chrome style tab switching bindings for bufferline

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -3,3 +3,12 @@ require("bufferline").setup({
 		always_show_bufferline = true, -- Ensure bufferline is always displayed
 	},
 })
+
+-- Close the current buffer
+vim.api.nvim_set_keymap('n', '<C-w>', ':bd<CR>', { noremap = true, silent = true })
+
+-- Switch to the left buffer
+vim.api.nvim_set_keymap('n', '<C-h>', ':BufferLineCyclePrev<CR>', { noremap = true, silent = true })
+
+-- Switch to the right buffer
+vim.api.nvim_set_keymap('n', '<C-l>', ':BufferLineCycleNext<CR>', { noremap = true, silent = true })
