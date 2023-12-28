d64a22369da54cf4ca64162d8954dd2634305f65
Daniël Terwiel <daniel@terwiel.io>
2023-12-28 15:38:30 +0100

    chore: always show gitsigns gutter

---
+++ after/plugin/gitsigns.lua
@@ -0,0 +1,12 @@
+local gitsigns = require("gitsigns")
+
+gitsigns.setup({
+	signcolumn = true, -- Always show the sign column
+	signs = {
+		add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+		change = { hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+		delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+		topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+		changedelete = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+	},
+})
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -58,9 +58,6 @@ return require("packer").startup(function(use)
 	use({
 		"lewis6991/gitsigns.nvim",
 		requires = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("gitsigns").setup()
-		end,
 	})
 
 	-- mason: managing LSP servers, linters, etc.
