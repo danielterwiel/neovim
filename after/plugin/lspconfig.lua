20cb2917a0c6ac03fe9c3930f293c5c6e7d5c109
Daniël Terwiel <daniel@terwiel.io>
2023-12-29 14:03:52 +0100

    fix: TypeScript LSP + React bug

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -24,3 +24,20 @@ lspconfig.tsserver.setup({
 lspconfig.cssls.setup({ on_attach = on_attach })
 lspconfig.html.setup({ on_attach = on_attach })
 lspconfig.jsonls.setup({ on_attach = on_attach })
+
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+        -- Customize diagnostic settings here
+        virtual_text = true,
+        signs = true,
+        underline = true,
+    }
+)
+
+-- Customize the diagnostic signs
+vim.fn.sign_define("LspDiagnosticsSignError", {text = "", texthl = "LspDiagnosticsSignError"})
+vim.fn.sign_define("LspDiagnosticsSignWarning", {text = "", texthl = "LspDiagnosticsSignWarning"})
+vim.fn.sign_define("LspDiagnosticsSignInformation", {text = "", texthl = "LspDiagnosticsSignInformation"})
+vim.fn.sign_define("LspDiagnosticsSignHint", {text = "", texthl = "LspDiagnosticsSignHint"})
+
